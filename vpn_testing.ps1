400c558a086307b6e28477a00445462d0ca01f07
mihai cziraki <58424830+czirakim@users.noreply.github.com>
2025-03-18 14:47:49 +0200

    Update vpn_testing.ps1

--- vpn_testing.ps1
+++ vpn_testing.ps1
@@ -21,6 +21,32 @@ $ssh_list = @( "192.168.1.12"
 
 $list_repsonse = @( "000" )
 
+function Get-Dns {
+
+	$defaultGateway = Get-NetRoute -DestinationPrefix "0.0.0.0/0" |  Where-Object -FilterScript { $_.NextHop -Ne "0.0.0.0" } |  Select-Object -ExpandProperty "NextHop"
+	
+	# Identify the default network interface
+	$defaultInterface = Get-NetRoute -DestinationPrefix "0.0.0.0/0" | Where-Object { $_.NextHop -eq $defaultGateway }
+
+	# Get the DNS server addresses for the default network interface
+	$dnsServers = Get-DnsClientServerAddress -InterfaceAlias $defaultInterface.InterfaceAlias
+		
+	# Display the default DNS servers
+	Write-Host -ForegroundColor Green "`n Default DNS Servers: $($dnsServers.ServerAddresses)"
+			
+	# Get all network configurations excluding the default interface
+	$networkConfigurations = Get-NetIPConfiguration | Where-Object { $_.InterfaceAlias -ne $defaultInterface.InterfaceAlias }
+	# Filter out configurations where the network adapter is disconnected or DNS server is null
+	$VpnNetworkConfigurations = $networkConfigurations | Where-Object { $_.NetAdapter.Status -ne "Disconnected" -and $_.DNSServer -ne $null }
+			
+	# Display the VPN DNS servers
+	if ($VpnNetworkConfigurations) {
+		# Get the DNS server addresses for the VPN network interface
+		$VpnDnsServers = Get-DnsClientServerAddress -InterfaceAlias $VpnNetworkConfigurations.InterfaceAlias
+		Write-Host -ForegroundColor RED "`n !!! If you are using a VPN, you might not be using the above default DNS servers !!!"
+		Write-Host -ForegroundColor Green "`n VPN DNS Servers: $($VpnDnsServers.ServerAddresses) `n"
+	}
+}
 function Get-DomainNameFromUrl {
     param (
         [string]$url
@@ -36,91 +62,107 @@ function Get-DomainNameFromUrl {
     return $domain
 }
 
-function Get-DomainNameStatusURL {
-Write-Host "  DNS:" -NoNewline	
-# Print each item in the url_list
-		try {
-			$dnsInfo = Resolve-DnsName (Get-DomainNameFromUrl -url $item) -ErrorAction Stop
-			Write-Host -ForegroundColor Green "OK"
-		} catch {
-			Write-Host -ForegroundColor Red "Failed"
-			}
-}
-
 function Get-DomainNameStatus {
-Write-Host "  DNS:" -NoNewline	
-# Print each item in the url_list
-		try {
-			$dnsInfo = Resolve-DnsName  $item -ErrorAction Stop
-			Write-Host -ForegroundColor Green "OK"
-		} catch {
-			Write-Host -ForegroundColor Red "Failed"
-			}
+    param (
+        [string]$url = $null,
+        [string]$domainName = $null
+    )
+
+    Write-Host "  DNS:" -NoNewline
+
+    try {
+        if ($url) {
+            $domainName = Get-DomainNameFromUrl -url $url
+        }
+        
+        $dnsInfo = Resolve-DnsName $domainName -ErrorAction Stop
+        Write-Host -ForegroundColor Green "OK"
+    } catch {
+        Write-Host -ForegroundColor Red "Failed"
+    }
 }
 
 function Test_url {
 # Print each item in the url_list
-Write-Host "`n Testing URLs: `n"
-foreach ($item in $url_list) {
-    Write-Host " $item " -NoNewline	-ForegroundColor Yellow
-	Write-Host "  StatusCode: " -NoNewline
-    $response = . curl -sk -m 10 -o /dev/null -w "%{http_code}" $item
-	if ($list_repsonse -notcontains $response) {
-		Write-Host -ForegroundColor Green $response -NoNewline
-	} else {
-		Write-Host -ForegroundColor Red "Failed" -NoNewline	
+	Write-Host "`n Testing URLs: `n"
+	
+	foreach ($item in $url_list) {
+		Write-Host " $item " -NoNewline	-ForegroundColor Yellow
+		Write-Host "  StatusCode: " -NoNewline
+ 
+		$response = . curl -sk -m 10 -o /dev/null -w "%{http_code}" $item
+
+		if ($list_repsonse -notcontains $response) {
+			Write-Host -ForegroundColor Green $response -NoNewline
+		} else {
+			Write-Host -ForegroundColor Red "Failed" -NoNewline	
+		}
+
+		Get-DomainNameStatus -url $item
 	}
-	Get-DomainNameStatusURL
-}
 }
 	
 function Test_share {
 # Print each item in the Share_list
-Write-Host "`n`n Testing Share Paths: `n"
-foreach ($item in $share_list) {
-    Write-Host " $item " -NoNewline -ForegroundColor Yellow
-	$response = Test-NetConnection  -ComputerName $item -CommonTCPPort SMB -InformationLevel "Quiet"
-	if ($response) {
-		Write-Host -ForegroundColor Green "OK" -NoNewline	
-	} else {
-		Write-Host -ForegroundColor Red "Failed" -NoNewline	
-	}
-	Get-DomainNameStatus
-}	
+	Write-Host "`n`n Testing Share Paths: `n"
+	
+	foreach ($item in $share_list) {
+		Write-Host " $item " -NoNewline -ForegroundColor Yellow
+	
+		$response = Test-NetConnection  -ComputerName $item -CommonTCPPort SMB -InformationLevel "Quiet"
+
+		if ($response) {
+			Write-Host -ForegroundColor Green "OK" -NoNewline	
+		} else {
+			Write-Host -ForegroundColor Red "Failed" -NoNewline	
+		}
+		
+		Get-DomainNameStatus -domainName $item
+	}	
 }
 
 function Test_rdp  {
 # Print each item in the rdp_list
-Write-Host "`n`n Testing RDP: `n"
-foreach ($item in $rdp_list) {
-    Write-Host " $item " -NoNewline -ForegroundColor Yellow
-	$response = Test-NetConnection  -ComputerName $item -CommonTCPPort RDP -InformationLevel "Quiet"
-	if ($response) {
-		Write-Host -ForegroundColor Green "OK" -NoNewline	
-	} else {
-		Write-Host -ForegroundColor Red "Failed" -NoNewline	
+	Write-Host "`n`n Testing RDP: `n"
+	
+	foreach ($item in $rdp_list) {
+		Write-Host " $item " -NoNewline -ForegroundColor Yellow
+	
+		$response = Test-NetConnection  -ComputerName $item -CommonTCPPort RDP -InformationLevel "Quiet"
+		
+		if ($response) {
+			Write-Host -ForegroundColor Green "OK" -NoNewline	
+		} else {
+			Write-Host -ForegroundColor Red "Failed" -NoNewline	
+		}
+	
+		Get-DomainNameStatus -domainName $item
 	}
-	Get-DomainNameStatus
-}
 }
 
 function Test_ssh  {
 # Print each item in the ssh_list
-Write-Host "`n`n Testing ssh: `n"
-foreach ($item in $ssh_list) {
-    Write-Host " $item " -NoNewline -ForegroundColor Yellow
-	$response = Test-NetConnection  -ComputerName $item -Port 22 -InformationLevel "Quiet"
-	if ($response) {
-		Write-Host -ForegroundColor Green "OK" -NoNewline	
-	} else {
-		Write-Host -ForegroundColor Red "Failed" -NoNewline	
+	Write-Host "`n`n Testing ssh: `n"
+	
+	foreach ($item in $ssh_list) {
+		Write-Host " $item " -NoNewline -ForegroundColor Yellow
+		
+		$response = Test-NetConnection  -ComputerName $item -Port 22 -InformationLevel "Quiet"
+	
+		if ($response) {
+			Write-Host -ForegroundColor Green "OK" -NoNewline	
+		} else {
+			Write-Host -ForegroundColor Red "Failed" -NoNewline	
+		}
+	
+		Get-DomainNameStatus -domainName $item
 	}
-	Get-DomainNameStatus
-}
 }
 
 ### main ###
 
+Write-Host -ForegroundColor Green "`n Starting tests ... "
+Get-Dns
 Test_url
 Test_share
 Test_rdp
